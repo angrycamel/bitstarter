0df27bcf3b91b38221103340e36e6799fa226a7a
angrycamel <angrycamel@edburcher.com>
2013-07-14 16:21:11 +0000

    read content from index.html

--- web.js
+++ web.js
@@ -1,11 +1,15 @@
+var fs = require('fs')
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var contentBuffer = fs.readFileSync("index.html");
+  var content = contentBuffer.toString("UTF-8"); 
+console.log(content);
+  
+  response.send(content);
 });
-
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
